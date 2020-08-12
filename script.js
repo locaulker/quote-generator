179dc09de7e558cb76cd789e4f49f60f474c9c74
Lawrence <Caulker>
2020-08-11 21:42:05 -0400

    completed project

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
+function showLoadingSpinner() {
+  loader.hidden = false
+  quoteContainer.hidden = true
+}
+
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false
+    loader.hidden = true
+  }
+}
 
 // Get Quote from API
 async function getQuote() {
+  showLoadingSpinner()
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-
   const apiUrl =
     'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
   try {
@@ -30,10 +43,23 @@ async function getQuote() {
     }
 
     quoteText.innerText = data.quoteText
+    removeLoadingSpinner()
   } catch (error) {
     getQuote()
   }
 }
+
+// Twitter Quote
+function tweetQuote() {
+  const quote = quoteText.innerText
+  const author = authorText.innerText
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+  window.open(twitterUrl, '_blank')
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
 // On Load
 getQuote()
